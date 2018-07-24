b74ba9f8bbe7046b88f4553c112ed7bd3395a923
Morior <moriorgames@gmail.com>
2018-07-24 07:28:30 +0200

    add controller and use case

--- src/App/Controller/PaymentController.php
+++ src/App/Controller/PaymentController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
+use App\UseCase\ExecutePaymentUseCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
 class PaymentController
 {
-
+    /**
+     * Get battle by token
+     *
+     * @Route("/execute-payment", methods={"POST"})
+     *
+     * @param ExecutePaymentUseCase $useCase
+     * @param Request               $request
+     *
+     * @return JsonResponse
+     */
+    public function executePayment(ExecutePaymentUseCase $useCase, Request $request)
+    {
+        return new JsonResponse(
+            $useCase->execute($request->request->all())
+        );
+    }
 }
---
+++ src/App/UseCase/ExecutePaymentUseCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\UseCase;
+
+class ExecutePaymentUseCase
+{
+    public function execute(array $data): array
+    {
+        return [];
+    }
+}
