c02b96805db5e8fac9c20c922de5994c02fe6da4
Morior <moriorgames@gmail.com>
2018-07-24 08:00:18 +0200

    Remove unnecessary dependencies from the project

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
